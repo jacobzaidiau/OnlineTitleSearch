e867afee6b118180115e9a882d9ec330fcdc4a02
jacobzaidi <jacob@zaidi.id.ua>
2020-03-15 20:30:16 +1000

    Added unit tests.

--- Context/SearchDbContext.cs
+++ Context/SearchDbContext.cs
@@ -9,6 +9,19 @@ namespace OnlineTitleSearch.Context
 {
     public class SearchDbContext : DbContext
     {
+
+        public SearchDbContext():base()
+        {
+            Database.SetInitializer<SearchDbContext>(null);
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
         public DbSet<Search> Searches { get; set; }
         public DbSet<Domain> Domains { get; set; }
         public DbSet<Result> Results { get; set; }
